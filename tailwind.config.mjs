4e179c93897807c0978d8f43ff48eaca6612cf72
Obliwobbi <tobiashartzberg@gmail.com>
2025-09-28 22:02:11 +0200

    Alot of messy business here, needs some reformatting in colors and borders etc.. hover function is messed up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: [
         './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}',
@@ -6,17 +8,18 @@ export default {
     theme: {
         extend: {
             colors: {
-                brand: {
-                    bg: '#FFD4D0',
-                    text: '#2C2C2C',
-                    primary: '#FFEED6',
-                    secondary: '#F5B7B1',
-                    accent: '#A7C7E7',
-                }
+                'brand-bg': '#FFD4D0',
+                'brand-text': '#b80101',
+                'brand-text-secondary': '#A7C7E7',
+                'brand-primary': '#FFEED6',
+                'brand-secondary': '#F5B7B1',
+                'brand-accent': '#A7C7E7',
+                'brand-footer': '#CDEAC0',
+                'brand-hover': '#97A13B',
             },
             fontFamily: {
-                heading: ['Poppins', 'ui-sans-serif', 'system-ui'],
-                body: ['Inter', 'ui-sans-serif', 'system-ui']
+                heading: ['StemmeForDyrene', ...defaultTheme.fontFamily.sans],
+                body: ['StemmeForDyrene', ...defaultTheme.fontFamily.sans],
             },
             boxShadow: {
                 soft: '0 10px 30px rgba(0,0,0,0.05)'
